004815c14e73e002d2d8ee5e001d843d41d948c6
Eduarda Silva <mariaedu_irt15@icloud.com>
2025-11-27 15:58:13 -0300

    Add files via upload

--- selects_filmes.sql
+++ selects_filmes.sql
@@ -37,26 +37,36 @@ ORDER BY duracao_minutos DESC
 LIMIT 10;
 
 -- 6. Eu quero saber quais são os filmes do Diretor Christopher Nolan
+SELECT * FROM filmes WHERE diretor= "Christopher Nolan"
 
 -- 7. Eu quero saber quais são os filmes do James Gunn
-
+SELECT * FROM filmes WHERE diretor= "James Gunn"
 -- 8. Eu quero saber quais os filmes de ação lançados em 2016
-
+SELECT * FROM filmes WHERE genero = "Ação" AND ano_lancamento = 2016
 -- 9. Eu quero saber quais os filmes do batman e quais os diretores
-
+SELECT diretor FROM filmes WHERE titulo like "%batman%"
 -- 10. Eu quero saber quais os filmes de terror e drama lançado entre 1980 e 2002
-
+SELECT * FROM filmes WHERE genero in ( "terror", "drama") 
+and ano_lancamento BETWEEN 1980 and 2002 ORDER by ano_lancamento
 -- 11. Eu quero Saber quantos filmes foram lançados em 1999
-
+SELECT ano_lancamento, titulo FROM filmes WHERE ano_lancamento = 1999
+-- para colocar para contar é só adicionar count(titulo) as total_titulo (só a contagem e adiciona antes do from)
 -- 12. Eu quero saber quantos filmes tem o diretor Quentin Tarantino
-
+SELECT diretor, titulo, ano_lancamento FROM filmes WHERE diretor = "Quentin Tarantino"
 -- 13. Eu quero saber quais os filmes do James Cameron e quais os generos deles
-
+SELECT diretor , titulo, genero from filmes  WHERE diretor = "James Cameron"
 -- 14. Por fim vamos adiconar as informações do seu filme favorito na tabela.
-
+INSERT into filmes (titulo, genero,  ano_lancamento, duracao_minutos, diretor, nota_imdb) VALUES
+( 'Gente grande', 'comédia', 2013, 100, 'Dennis Dugan', 7.0) 
+ 
 -- 15. Vamos adiconar as informações do filme que o professor odeia.
-
+INSERT into filmes (titulo, genero,  ano_lancamento, duracao_minutos, diretor, nota_imdb) VALUES
+('homen-aranha: longe de casa', 'Ação', 2019, 130, 'Jon Watts', 7.4)
 -- 16. Vamos deletar esse filme que o professor detesta.
+DELETE FROM filmes WHERE id = 103
+
+select * from filmes order by id desc
+
 
 
 
